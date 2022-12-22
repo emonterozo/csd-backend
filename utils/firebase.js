4339d04e1ae7c25f50084eb769b71790b38c0860
Eric Monterozo <eqmonterozo@findme.com.ph>
2022-12-22 15:15:29 +0800

    remove

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 // Initialize firebase admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(
-    "./config/project-csd-ec082-firebase-adminsdk-mjv0o-8646c43390.json"
+    "../config/project-csd-ec082-firebase-adminsdk-mjv0o-8646c43390.json.json"
   ),
   storageBucket: "project-csd-ec082.appspot.com",
 });
