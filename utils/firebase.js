311a63f6387a80444f21bad51e642ccabf86696c
Eric Monterozo <eqmonterozo@findme.com.ph>
2022-12-22 15:31:29 +0800

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
+const path = require("path");
 
 // Initialize firebase admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert("../config/project_csd.json"),
+  credential: admin.credential.cert(
+    path.resolve(__dirname, "../config/project_csd.json")
+  ),
   storageBucket: "project-csd-ec082.appspot.com",
 });
 // Cloud storage
