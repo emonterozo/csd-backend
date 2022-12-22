cce1c899e84647aaad41eefe99ebf7b7618a09eb
Eric Monterozo <eqmonterozo@findme.com.ph>
2022-12-22 15:19:54 +0800

    rename json

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,9 +2,7 @@ const admin = require("firebase-admin");
 
 // Initialize firebase admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(
-    "../config/project-csd-ec082-firebase-adminsdk-mjv0o-8646c43390.json.json"
-  ),
+  credential: admin.credential.cert("../config/project_csd.json"),
   storageBucket: "project-csd-ec082.appspot.com",
 });
 // Cloud storage
