894917c43cc2484099a83796a68f6c606ce14e0d
Eric Monterozo <ric.monterozo@gmail.com>
2023-01-06 16:48:57 +0800

    define mongoose

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const { jwtSign, uploadFile, uploadToStorage } = require("../utils/utils");
 
